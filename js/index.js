dbbeae7e389d70f3c2bc3875030b3aa55b20406e
CamiloHuesa <camilowch@gmail.com>
2021-07-28 16:32:17 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,67 @@
+window.sr = ScrollReveal();
+
+	sr.reveal('.spot', {
+		duration: 8000,
+		origin: 'top',
+		distance: '-100px',
+	});
+
+	sr.reveal('.box6', {
+		duration: 8000,
+		origin: 'right',
+		distance: '-600px'
+	});
+
+	sr.reveal('.texto1', {
+		duration: 8000,
+		origin: 'right',
+		distance: '-600px'
+	});
+
+	sr.reveal('.sup2', {
+		duration: 8000,
+		origin: 'left',
+		distance: '-600px'
+	});
+
+	sr.reveal('.integrantes', {
+		duration: 4000,
+		origin: 'top',
+		distance: '-600px'
+	});
+
+	sr.reveal('.sup3', {
+		duration: 8000,
+		origin: 'left',
+		distance: '-600px'
+	});
+	sr.reveal('.video2', {
+		duration: 8000,
+		origin: 'left',
+		distance: '-600px'
+	});
+	sr.reveal('.video3', {
+		duration: 8000,
+		origin: 'right',
+		distance: '-600px'
+	});
+	sr.reveal('.inferior', {
+		duration: 8000,
+		origin: 'left',
+		distance: '-600px'
+	});
+	sr.reveal('.perfil', {
+		duration: 8000,
+		origin: 'right',
+		distance: '500px'
+	});
+	sr.reveal('.frase', {
+		duration: 8000,
+		origin: 'bottom',
+		distance: '700px'
+	});
+	sr.reveal('.red', {
+		duration: 8000,
+		origin: 'left',
+		distance: '700px'
+	});
